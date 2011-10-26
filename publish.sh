309c75fb168c2508ce8af500bb5d1dc52f920dd5
UENISHI Kota <uenishi.kota@lab.ntt.co.jp>
2011-10-27 01:28:14 +0900

    wrong script fixed

--- publish.sh
+++ publish.sh
@@ -2,13 +2,14 @@
 
 omake html
 cd build
-rm -rf jubatusi.github.com/_static
+rm -rf jubatus.github.com/_static
 rm -rf jubatus.github.com/_sources
-cp -r html/* jubatus.github.com/
+cp -r html/* jubatus.github.com
 cd jubatus.github.com
 git add *.html
 git add _static/*
 git add _sources/*
+git add _images/*
 git commit -m "autocommit by publish.sh"
 git push
 
