5a24347cef68d26821cbdf54192ab1be4b5713a1
ODA Satoshi <oda.satoshi@gmail.com>
2012-07-02 09:09:59 +0900

    fix directory structure changed

--- publish.sh
+++ publish.sh
@@ -6,7 +6,8 @@ rm -rf jubatus.github.com/_static
 rm -rf jubatus.github.com/_sources
 cp -r html/* jubatus.github.com
 cd jubatus.github.com
-git add *.html
+git add en/*.html
+git add jp/*.html
 git add _static/*
 git add _sources/*
 git add _images/*
