8c1fac5b3608dd78cee37e83afe0e574944bfaf9
IMAMASU Ryohei <imms.ryohei@gmail.com>
2012-10-23 18:23:37 +0900

    fix #14

--- publish.sh
+++ publish.sh
@@ -4,8 +4,10 @@ omake html
 cd build
 rm -rf jubatus.github.com/_static
 rm -rf jubatus.github.com/_sources
+LANG=C diff html jubatus.github.com | egrep '^Only in jubatus.github.com' | awk '{print $4}' | grep '^[^\.A-Z].*$' | xargs -i rm -rf jubatus.github.com/{}
 cp -r html/* jubatus.github.com
 cd jubatus.github.com
+git status | grep deleted: | awk '{print $3}' | xargs git rm -r
 git add *.html
 git add en/*.html
 git add ja/*.html
