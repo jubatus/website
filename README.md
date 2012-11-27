464dcf6c6be00ff88782fa6a7010b1c2de7b8db1
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2012-11-27 13:37:14 +0900

    new flow to update website

---
+++ README.md
@@ -0,0 +1,61 @@
+Jubatus Website
+===============
+
+
+About
+-----
+
+This repository holds Sphinx source files for http://jubat.us/ website.
+
+Contributions are always welcomed, from tiny typos to enhancements.
+Feel free to make pull-requests to this repository.
+
+
+Requirements
+------------
+
+* Sphinx 1.1.3
+    * `python-sphinx` package is available on Ubuntu
+
+
+How to Edit
+-----------
+
+First of all, clone this repository (if you don't have a commit access, use your fork).
+
+```
+$ git clone git@github.com:jubatus/website.git
+```
+
+Edit the document.
+
+```
+$ edit source/index.rst
+```
+
+Build and preview the changes you made.
+
+```
+$ omake html
+$ open build/html/index.html
+```
+
+Looks good? Now, push it.
+
+```
+$ git push origin master
+```
+
+Then open a pull-request.
+
+For Jubatus members: you can deploy it to http://jubat.us/ website.
+
+```
+$ ./publish.sh
+```
+
+
+Copyright
+---------
+
+(C) PFI & NTT
--- publish.sh
+++ publish.sh
@@ -1,19 +1,23 @@
-#!/bin/sh
+#!/bin/bash -ue
 
-omake html
-cd build
-rm -rf jubatus.github.com/_static
-rm -rf jubatus.github.com/_sources
-LANG=C diff html jubatus.github.com | egrep '^Only in jubatus.github.com' | awk '{print $4}' | grep '^[^\.A-Z].*$' | xargs -i rm -rf jubatus.github.com/{}
-cp -r html/* jubatus.github.com
-cd jubatus.github.com
-git status | grep deleted: | awk '{print $3}' | xargs git rm -r
-git add *.html
-git add en/*.html
-git add ja/*.html
-git add _static/*
-git add _sources/*
-git add _images/*
+WEBROOT="jubatus.github.com"
+
+omake clean html
+
+if [ ! -d "${WEBROOT}" ]; then
+	git clone git@github.com:jubatus/jubatus.github.com.git "${WEBROOT}"
+	git checkout master
+fi
+
+pushd "${WEBROOT}"
+
+find . -mindepth 1 -and \
+	! \( -path "./.git*" -or -name .nojekyll -or -name CNAME \) \
+	-delete
+
+cp -a ../build/html/* .
+git add -A
 git commit -m "autocommit by publish.sh"
-git push
+git push origin master
 
+popd
