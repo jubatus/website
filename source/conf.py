4e5d98b4995f8f16bd463521e6d58ebb702e4a2c
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2017-02-21 18:04:17 +0900

    bump copyright year to 2017

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Jubatus'
-copyright = u'2011-2016 PFN & NTT'
+copyright = u'2011-2017 PFN & NTT'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
